5d5b03dd77ee8c5e5c42d14fb6573eeb03b75d4a
Adam Pitchie <adampitchie@gmail.com>
2021-03-09 12:33:17 -0800

    made some progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
-// Your code goes here
+// click
+const button = document.querySelector(".btn")
+
+button.addEventListener('click', function(event) {
+  const color = document.body.style.backgroundColor
+  document.body.style.backgroundColor = color === 'white' ? 'blue' : 'white'
+})
+
+// mouseover
+const img = document.querySelector('img')
+
+img.addEventListener('mouseover', function(event) {
+  const color = document.body.style.backgroundColor
+  document.body.style.backgroundColor = color === 'white' ? 'green' : 'white'
+})
+
+// dblclick
+document.addEventListener('dblclick', function(event) {
+  const color = document.body.style.backgroundColor
+  document.body.style.backgroundColor = color === 'white' ? 'pink' : 'white'
+})
+
+// keydown
+document.addEventListener('keydown', function(event) {
+  const color = document.body.style.backgroundColor
+  document.body.style.backgroundColor = color === 'white' ? 'teal' : 'white'
+})
+
+// keyup
+document.addEventListener('keyup', function(event) {
+  document.body.style.backgroundColor = 'white'
+})
+
+// scroll
+document.addEventListener('scroll', function(event) {
+  const color = document.body.style.backgroundColor
+  document.body.style.backgroundColor = color === 'white' ? 'azure' : 'white'
+})
+
+// wheel
+document.addEventListener('wheel', function(event) {
+  const color = document.body.style.backgroundColor
+  document.body.style.backgroundColor = color === 'white' ? 'aqua' : 'white'
+  event.stopPropagation();
+})
+
+// resize
+window.addEventListener('resize', function(event) {
+  console.log(event.timeStamp)
+})
+
+// prevent links from refreshing page
+Array.from(document.links).forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+  })
+})
